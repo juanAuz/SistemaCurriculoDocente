7fad81ffa15afe652320a4c276b78c433f731f4b
Evan Matango <evan.matango@epn.edu.ec>
2026-01-23 03:29:09 -0500

    Agrego clases a capacitacion y borrador de plantilla

---
+++ src/main/java/modelo/capacitacion/CapacitacionImpartida.java
@@ -0,0 +1,27 @@
+package modelo.capacitacion;
+
+import java.time.LocalDate;
+
+public class CapacitacionImpartida extends Capacitacion {
+    // Atributo específico
+    private String publicoDirigido; // Ej: "Estudiantes", "Profesionales"
+
+    public CapacitacionImpartida(String tema, String institucion, LocalDate fechaInicio, LocalDate fechaFin, int numeroHoras, String publicoDirigido) {
+        super(tema, institucion, fechaInicio, fechaFin, numeroHoras);
+        this.publicoDirigido = publicoDirigido;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Impartida";
+    }
+
+    // Getters y Setters específicos
+    public String getPublicoDirigido() { return publicoDirigido; }
+    public void setPublicoDirigido(String publicoDirigido) { this.publicoDirigido = publicoDirigido; }
+
+    @Override
+    public String toString() {
+        return super.getTema() + " (Dictada a: " + publicoDirigido + ")";
+    }
+}
